7b6a41666844515c77d70677b47afa2b81fb6218
gmaggiotti <gabriel.maggiotti@mercadolibre.com>
2016-11-13 10:02:03 -0300

    adding logger

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ var restify = require('restify');
 var bodyparser = require('body-parser');
 var connection = require('./connection');
 var routes = require('./routes/routes');
+var logger = require('restify-logger');
 
 var app = restify.createServer();
+
+app.use(logger('custom', {
+    skip: function (req) {
+        return process.env.NODE_ENV === "test" || req.method === "OPTIONS" || req.url === "/status";
+    }
+}));
+
 app.use(restify.bodyParser());
 
 connection.init();
